714ed3af7598ae3ac458ef7c3145d3f831e04e04
Burr Sutter <burrsutter@gmail.com>
2015-04-11 00:47:37 +0000

    adding ir_temperature.js

---
+++ ir_temperature.js
@@ -0,0 +1,21 @@
+var SensorTag = require('sensortag');
+
+SensorTag.discover(function(device) {
+  console.log('discovered device with UUID: ' + device['uuid']);
+  device.connect(function() {
+    console.log('connected');
+    device.discoverServicesAndCharacteristics(function() {
+      console.log('discovered services and characteristics');
+      device.enableIrTemperature(function() {
+        device.on('irTemperatureChange', function(objectTemperature, ambientTemperature) {
+          console.log('\tobject temperature = %d °C', objectTemperature.toFixed(1));
+          console.log('\tambient temperature = %d %', ambientTemperature.toFixed(1));
+        });
+
+        device.notifyIrTemperature(function() {
+          console.log('notifyIrTemperature');
+        });
+      }); // enableIrTemperature
+    }); // discoverServicesAndCharacteristics
+  }); // connect
+}); // discover
